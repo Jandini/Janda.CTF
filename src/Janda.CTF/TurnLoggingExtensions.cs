7fa691b98b68038d089f66ae779ad8daf4dfb8f7
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-25 21:49:30 +0000

    feat: Add right turn start-up container

---
+++ src/Janda.CTF/TurnLoggingExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using RightTurn;
+using System;
+
+namespace Janda.CTF
+{
+    internal static class TurnLoggingExtensions
+    {
+        public static ITurn WithLogging(this ITurn turn, Action<ILoggingBuilder, ITurn> loggingWithTurn)
+        {
+            turn.Directions.Add<Action<ILoggingBuilder>>((logging) => loggingWithTurn.Invoke(logging, turn));
+            return turn;
+        }
+    }
+}
