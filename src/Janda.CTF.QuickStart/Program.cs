727523e14559139715dd47591d44d5308735c577
Matt Janda <matt.janda.kingston@gmail.com>
2021-01-27 20:16:16 +0000

    chore: Upgrade right turn serilog extensions

--- src/Janda.CTF.QuickStart/Program.cs
+++ src/Janda.CTF.QuickStart/Program.cs
@@ -5,7 +5,7 @@ namespace Janda.CTF.QuickStart
 {
     class Program
     {
-        [CTF(Name = "Quick Start", MaximizeConsole = true)]
+        [CTF(Name = "Quick Start", MaximizeConsole = false)]
         static void Main(string[] args) => CTF.Run(args, (services) =>
         {
             services.AddTransient<IFlagFinder, FlagFinder>();
