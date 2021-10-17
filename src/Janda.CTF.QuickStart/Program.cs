cbdd10e418151a6d369828e0e248c3a096a4b0f5
Matt Janda <matt.janda.kingston@gmail.com>
2021-10-17 18:26:50 +0100

    feat: Add support for multiple ctf projects within one solution

--- src/Janda.CTF.QuickStart/Program.cs
+++ src/Janda.CTF.QuickStart/Program.cs
@@ -5,7 +5,7 @@ namespace Janda.CTF.QuickStart
 {
     class Program
     {
-        [CTF(Name = "Quick Start", MaximizeConsole = false)]
+        [CTF(Name = "Quick Start", MaximizeConsole = false, ProjectName = "Janda.CTF.QuickStart")]
         static void Main(string[] args) => CTF.Run(args, (services) =>
         {
             services.AddTransient<IFlagFinder, FlagFinder>();
